3485257983fb136727be492105abd56b22f1b7fb
Le Anh Khanh <leanhkhanh6540@gmail.com>
2023-03-18 22:30:24 +0700

    hello

--- frontend/src/components/Input/CompInput.vue
+++ frontend/src/components/Input/CompInput.vue
@@ -20,6 +20,7 @@ function handleAddTodo() {
   });
   fetch("http://localhost:3000/users/",{
     method: "POST",
+    headers: { "Content-Type": "application/json", },
     body: JSON.stringify({
       name:"khanh",
       age:10,
